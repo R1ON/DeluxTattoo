466dab7957092d9d60ea1ac932d1187216238382
roman <r.maximov2012@yandex.ru>
2017-12-27 22:22:47 +0300

    сделал нормальный раздел с фотками и адаптив

--- src/Pages/Home/Components/MainComponent.js
+++ src/Pages/Home/Components/MainComponent.js
@@ -24,17 +24,17 @@ class MainComponent extends Component {
         </Row>
 
         <Row>
-          <Col md={4}>
+          <Col md={4} sm={6} xs={12}>
             <Icon className="home-main-icon" size="5x" name="thumbs-up" />
             <div className="home-main-icon-title">GREAT QUALITY</div>
             <div className="home-main-icon-about">We have the best masters and extensive experience behind the shoulders</div>
           </Col>
-          <Col md={4}>
+          <Col md={4} sm={6} xs={12}>
             <Icon className="home-main-icon" size="5x" name="clock-o" />
             <div className="home-main-icon-title">FAST</div>
             <div className="home-main-icon-about">Experience of masters allows you to make tattoos much faster</div>
           </Col>
-          <Col md={4}>
+          <Col md={4} sm={12} xs={12}>
             <Icon className="home-main-icon" size="5x" name="shower" />
             <div className="home-main-icon-title">CLEAN STUDIO</div>
             <div className="home-main-icon-about">Clean studio, sterile equipment, disposable needles</div>
@@ -42,17 +42,17 @@ class MainComponent extends Component {
         </Row>
 
         <Row>
-          <Col md={4}>
+          <Col md={4} sm={6} xs={12}>
             <Icon className="home-main-icon" size="5x" name="wifi" />
             <div className="home-main-icon-title">FREE WI-FI</div>
             <div className="home-main-icon-about">To get distracted during the session, you can use our internet</div>
           </Col>
-          <Col md={4}>
+          <Col md={4} sm={6} xs={12}>
             <Icon className="home-main-icon" size="5x" name="vcard-o" />
             <div className="home-main-icon-title">GRADUATED MASTERS</div>
             <div className="home-main-icon-about">Each master has a certificate of completion of tattooing courses</div>
           </Col>
-          <Col md={4}>
+          <Col md={4} sm={12} xs={12}>
             <Icon className="home-main-icon" size="5x" name="coffee" />
             <div className="home-main-icon-title">FREE COFFEE</div>
             <div className="home-main-icon-about">If you are very hurt, you can ask the master to pour you coffee</div>
@@ -82,20 +82,51 @@ class MainComponent extends Component {
           <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/1.jpg')} /></div>
           <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/2.jpg')} /></div>
           <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/3.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/4.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/5.jpg')} /></div>
+          <div className="home-main-examples sm-disable"><img src={require('../../../Assets/img/WorkExamples/4.jpg')} /></div>
+          <div className="home-main-examples sm-disable"><img src={require('../../../Assets/img/WorkExamples/5.jpg')} /></div>
           <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/6.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/7.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/8.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/9.jpg')} /></div>
+          <div className="home-main-examples sm-disable"><img src={require('../../../Assets/img/WorkExamples/7.jpg')} /></div>
+          <div className="home-main-examples "><img src={require('../../../Assets/img/WorkExamples/8.jpg')} /></div>
+          <div className="home-main-examples xs-disable"><img src={require('../../../Assets/img/WorkExamples/9.jpg')} /></div>
           <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/10.jpg')} /></div>
-          <div className="home-main-examples"><img src={require('../../../Assets/img/WorkExamples/11.jpg')} /></div>
-          <div className="home-main-examples fix-masonry-1"><img src={require('../../../Assets/img/WorkExamples/12.jpg')} /></div>
-          <div className="home-main-examples fix-masonry-2"><img src={require('../../../Assets/img/WorkExamples/13.jpg')} /></div>
-          <div className="home-main-examples fix-masonry-3"><img src={require('../../../Assets/img/WorkExamples/14.jpg')} /></div>
+          <div className="home-main-examples sm-disable"><img src={require('../../../Assets/img/WorkExamples/11.jpg')} /></div>
+          <div className="home-main-examples xs-disable"><img src={require('../../../Assets/img/WorkExamples/12.jpg')} /></div>
+          <div className="home-main-examples sm-disable"><img src={require('../../../Assets/img/WorkExamples/13.jpg')} /></div>
+          <div className="home-main-examples xs-disable"><img src={require('../../../Assets/img/WorkExamples/14.jpg')} /></div>
         </Masonry>
+        <div className="home-main-masonry-border"></div>
 
-        <div className="test"></div>
+        <Row className="test">
+          <Col md={12}>
+            <div className="home-main-title">
+              WORK EXAMPLES
+            </div>
+          </Col>
+          <Col md={12}>
+            <div className="home-main-tagline">
+              View all works by <a href="">reference</a>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="test">
+          <Col md={4} sm={6} xs={12}>
+            <Icon className="home-main-icon" size="5x" name="wifi" />
+            <div className="home-main-icon-title">FREE WI-FI</div>
+            <div className="home-main-icon-about">To get distracted during the session, you can use our internet</div>
+          </Col>
+          <Col md={4} sm={6} xs={12}>
+            <Icon className="home-main-icon" size="5x" name="vcard-o" />
+            <div className="home-main-icon-title">GRADUATED MASTERS</div>
+            <div className="home-main-icon-about">Each master has a certificate of completion of tattooing courses</div>
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <Icon className="home-main-icon" size="5x" name="coffee" />
+            <div className="home-main-icon-title">FREE COFFEE</div>
+            <div className="home-main-icon-about">If you are very hurt, you can ask the master to pour you coffee</div>
+          </Col>
+        </Row>
+
       </div>
     );
   }
