fb6ed92863fef1bdd18f550909f5ac51d5a2a04c
roman <r.maximov2012@yandex.ru>
2018-01-11 23:49:25 +0300

    Рефакторинг

--- src/Pages/Home/Components/FooterComponent.js
+++ src/Pages/Home/Components/FooterComponent.js
@@ -4,6 +4,8 @@ import { Icon } from 'react-fa';
 import { Carousel } from 'react-responsive-carousel';
 import StarRatings from 'react-star-ratings';
 
+import { COMMENTS } from '../Constants';
+
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 class FooterComponent extends Component {
@@ -56,40 +58,11 @@ class FooterComponent extends Component {
           showThumbs={false}
           emulateTouch={true}
         >
-          {this.reviewComponent(
-            `photo1`, `Rudolph Crawford`,
-            `I did the tattoo in your salon and was satisfied with the quality of the work.
-             After the session, I was served free coffee and given a cookie :)`,
-            3, 4
-          )}
-
-          {this.reviewComponent(
-            `photo2`, `Abigail Terry`,
-            `I did my first tattoo.
-             It was very painful, so the master worked quickly.
-             I thought it would be worse because of this, but it turned out great.`,
-            4, 5
-          )}
-
-          {this.reviewComponent(
-            `photo3`, `Oliver Wilson`,
-            `I saved up money and made another tattoo in this studio.
-             This is already 6 and I'm not going to stop. Highly recommend.`,
-            4, 3
-          )}
-
-          {this.reviewComponent(
-            `photo4`, `Flora Allison`,
-            `I did one more tattoo in your studio and I was very pleased, thank you.`,
-            4, 4
-          )}
-
-          {this.reviewComponent(
-            `photo5`, `Mark Smith`,
-            `Many thanks for the quality work,
-             I still do not believe that I did the tattoo, but still it was worth it.`,
-            5, 5
-          )}
+          {COMMENTS.map(comment => (
+            this.reviewComponent(
+              comment.img, comment.title, comment.text, comment.quality, comment.price
+            )
+          ))}
         </Carousel>
       </div>
     );
--- src/Pages/Home/Constants/index.js
+++ src/Pages/Home/Constants/index.js
@@ -2,3 +2,45 @@ export const IS_OPEN_MODAL = 'REGISTRATION_MODAL';
 export const IS_OPEN_IMAGE_MODAL = 'IS_OPEN_IMAGE_MODAL';
 export const SWITCH_IMAGE_LEFT = 'SWITCH_IMAGE_LEFT';
 export const SWITCH_IMAGE_RIGHT = 'SWITCH_IMAGE_RIGHT';
+
+export const COMMENTS = [
+  {
+    img: 'photo1',
+    title: 'Rudolph Crawford',
+    text: `I did the tattoo in your salon and was satisfied with the quality of the work. 
+    After the session, I was served free coffee and given a cookie :)`,
+    quality: 3,
+    price: 4
+  },
+  {
+    img: 'photo2',
+    title: 'Abigail Terry',
+    text: `I did my first tattoo. It was very painful, so the master worked quickly.
+    I thought it would be worse because of this, but it turned out great.`,
+    quality: 4,
+    price: 5
+  },
+  {
+    img: 'photo3',
+    title: 'Oliver Wilson',
+    text: `I saved up money and made another tattoo in this studio.
+    This is already 6 and I'm not going to stop. Highly recommend.`,
+    quality: 4,
+    price: 3
+  },
+  {
+    img: 'photo4',
+    title: 'Flora Allison',
+    text: `I did one more tattoo in your studio and I was very pleased, thank you.`,
+    quality: 4,
+    price: 4
+  },
+  {
+    img: 'photo5',
+    title: 'Mark Smith',
+    text: `Many thanks for the quality work,
+    I still do not believe that I did the tattoo, but still it was worth it.`,
+    quality: 5,
+    price: 5
+  },
+];
